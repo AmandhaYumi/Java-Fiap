00578afc995d92f23e6b285a496ec5c48eba4156
AmandhaYumi <mandhayumi@gmail.com>
2025-05-16 07:53:42 -0300

    Atualizando listas

--- ddd-livraria/src/br/com/fiap/models/Livro.java
+++ ddd-livraria/src/br/com/fiap/models/Livro.java
@@ -2,11 +2,12 @@ package br.com.fiap.models;
 
 
 import br.com.fiap.enums.TipoCapaEnum;
+import br.com.fiap.interfaces.Exemplar;
 
 import java.time.LocalDateTime;
 import java.util.Random;
 
-public class Livro {
+public abstract class Livro implements Exemplar {
     //Atributo tipo nomeAtributo;
     protected String titulo;
     private String autor;
@@ -14,7 +15,7 @@ public class Livro {
     private Editora editora;
     private int paginas;
     private String resumo;
-    private TipoCapaEnum tipoCapa; //COMUM, DURA, PERSONALIZADA
+
 
     //+ aplicarDesconto(taxa: double): double
     public double aplicarDesconto(double taxa){
@@ -22,6 +23,10 @@ public class Livro {
             return preco - preco * taxa / 100;
         return preco;
     }
+
+    public Livro() {
+    }
+
     public String getTitulo() {
         return titulo;
     }
@@ -70,41 +75,31 @@ public class Livro {
         this.resumo = resumo;
     }
 
-    public TipoCapaEnum getTipoCapa() {
-        return tipoCapa;
-    }
-
-    public void setTipoCapa(TipoCapaEnum tipoCapa) {
-        this.tipoCapa = tipoCapa;
-    }
 
     //Construtor
     //Sobrecarga de métodos -> Métodos com mesmo nome porém assinatura diferente
-    public Livro(){
-        tipoCapa = TipoCapaEnum.COMUM;
-    }
+
 
     public Livro(Editora editora){
         this.editora = editora;
-        tipoCapa = TipoCapaEnum.COMUM;
     }
 
     public Livro(String titulo, String autor, double preco,
-                 Editora editora, int paginas, String resumo,
-                 TipoCapaEnum tipoCapa) {
+                 Editora editora, int paginas, String resumo
+                 ) {
         this.titulo = titulo;
         this.autor = autor;
         this.preco = preco;
         this.editora = editora;
         this.paginas = paginas;
         this.resumo = resumo;
-        this.tipoCapa = tipoCapa;
+
     }
 
-    public Livro(String titulo, Editora editora, TipoCapaEnum tipoCapa) {
+    public Livro(String titulo, Editora editora) {
         this.titulo = titulo;
         this.editora = editora;
-        this.tipoCapa = tipoCapa;
+
     }
 
     //Método para exibir o livro
@@ -113,12 +108,15 @@ public class Livro {
         System.out.println("Autor: " + autor);
         System.out.println("Editora: " + editora.getNome());
         System.out.println("Preço: " + preco);
-        System.out.println("Tipo de capa: " + tipoCapa);
+        exibirDetalhes();
         System.out.println("-------------------------------------\n");
     }
-
+    public abstract void exibirDetalhes();
     public String exibirTipoCapa(){
         return "";
+    }
 
+    public boolean ehMaisCaroQue(Livro outroLivro){
+        return preco > outroLivro.preco;
     }
 }
